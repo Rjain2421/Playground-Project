5ef6cd72253f7cc50ddb4e61e0b0c0b075af765d
Rjain2421 <rmj2421@gmail.com>
2021-07-02 19:17:07 +0530

    deploy v1

---
+++ Frontend/src/components/Pdfs.js
@@ -0,0 +1,15 @@
+import Pdf from "./Pdf"
+
+const Pdfs = ({list_pdfs}) => {
+    let position=0;
+    return (
+        <div >
+        {list_pdfs.map((pdf)=>(
+           <div id="row"><Pdf key={pdf._id} pdf={pdf} position={position=position+1}/> </div>
+        ))}
+        </div>
+        
+    )
+}
+
+export default Pdfs
